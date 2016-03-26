2dca12405b15cf668d439fe091b171d58448fb74
Tomasz Pluskiewicz <tpluscode@user.noreply.github.com>
2016-03-26 20:45:06 +0100

    simplify

--- Deps/Registrations.cs
+++ Deps/Registrations.cs
@@ -8,27 +8,8 @@ namespace Deps
     {
         public Registrations()
         {
-            Register<IService>(typeof(Service));
             RegisterAll<IDep>(Lifetime.PerRequest);
         }
-    }
-
-    public class Module : NancyModule
-    {
-        public Module(IService ser)
-        {
-        }
-    }
-
-    public class Service : IService
-    {
-        public Service(IEnumerable<IDep> deps)
-        {
-        }
-    }
-
-    public interface IService
-    {
     }
 
     public interface IDep
