cbfb83c256bcb322334efca77ae11e26e7dc517a
Tomasz Pluskiewicz <tpluscode@user.noreply.github.com>
2016-03-26 20:13:15 +0100

    reproduction

---
+++ RegisterAllTest/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Deps;
+using Microsoft.Owin;
+using Nancy;
+using Owin;
+using RegisterAllTest;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace RegisterAllTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+
+    public class TestModule : NancyModule
+    {
+        public TestModule(IEnumerable<IDep> allDeps)
+        {
+            Get[string.Empty] = _ =>
+            {
+                var array = allDeps.ToArray();
+                return array.Length.ToString();
+            };
+        }
+    }
+
+    public class Dep1 : IDep { }
+    public class Dep2 : IDep { }
+    public class Dep3 : IDep { }
+    public class Dep4 : IDep { }
+}
